1cfa4c66761acc07a332b9e38ce7c2d9ad3e4b27
Bohdan Filimonyuk <bogdan1999kovel@gmail.com>
2019-05-12 22:18:59 +0200

    Added health for enemy

--- ProjectWSIZ/Game#666/Assets/Scripts/EnemyAI.cs
+++ ProjectWSIZ/Game#666/Assets/Scripts/EnemyAI.cs
@@ -3,12 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
+    public GameObject bar;
+    float damage = 10;
+    HealthSystem enemyHealth;
+    private float wait_timer;
+    private float wait_time = 2;
 
-	
+    void Start()
+    {
+        enemyHealth = gameObject.GetComponent<HealthSystem>();
+        gameObject.SendMessage("SetHp", 100);
+    }
 
-	void Start () {
-		
-	}
-	
-	
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player" && gameObject.GetComponent<BoxCollider2D>().isTrigger)
+    //    {
+    //        collision.gameObject.SendMessage("Damage", damage);
+    //    }
+    //}
+
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Player" && gameObject.GetComponent<BoxCollider2D>().isTrigger)
+    //    {
+    //        if (wait_timer < wait_time)
+    //            wait_timer += Time.deltaTime;
+    //        else
+    //        {
+    //            collision.gameObject.SendMessage("Damage", damage);
+    //            wait_timer = 2;
+    //        }
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.SendMessage("Damage", damage);
+        }
+    }
+    private void Update()
+    {
+        wait_time -= Time.deltaTime;
+        bar.transform.localScale = new Vector3(enemyHealth.GetHealthPercent(), 1f);
+    }
+    
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && wait_time < 0)
+        {
+            collision.gameObject.SendMessage("Damage", damage);
+            wait_time = 2;
+        }
+    }
 }
--- ProjectWSIZ/Game#666/Assets/Scripts/PlayerManager.cs
+++ ProjectWSIZ/Game#666/Assets/Scripts/PlayerManager.cs
@@ -6,7 +6,6 @@ public class PlayerManager : MonoBehaviour {
     public GameObject bar;
     float damage = 10;
     HealthSystem playerHealth;
-    float healthPerc;
 
     // Use this for initialization
     void Start () {
@@ -14,18 +13,17 @@ public class PlayerManager : MonoBehaviour {
         gameObject.SendMessage("SetHp", 100);
     }
 	
-	// Update is called once per frame
 	void Update () {
-        float healthPerc = playerHealth.GetHealthPercent();
-        bar.transform.localScale = new Vector3(healthPerc, 1f);
+        bar.transform.localScale = new Vector3(playerHealth.GetHealthPercent(), 1f);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy" && collision.gameObject.GetComponent<BoxCollider2D>().isTrigger)
-        {
-            Debug.Log("IS TRIGGGGEEEERRRREEEEED");
-            gameObject.SendMessage("Damage", damage);
-        }
-    }
+
+    //DAMAGE
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Enemy")
+    //    {
+    //        collision.gameObject.SendMessage("Damage", damage);
+    //    }
+    //}
 }
